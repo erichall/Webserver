4e87827c8667a7d97756963db375b962e9fd0eba
erichall <hallstrom.eric@gmail.com>
2015-12-03 16:47:43 +0100

    Some updated with client

--- ATMClient.go
+++ ATMClient.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"net"
 	//"strings"
+	"encoding/binary"
+	"strconv"
+	"math"
 )
 
 func main() {
@@ -32,12 +35,39 @@ func getAddr() (string, string) {
 	return ip, port
 }
 
-
+func write(connection net.Conn, msg []byte) {
+	size := binary.Size(msg)
+	//fmt.Println(size)
+	var tmpMsg = make([]byte, 10)
+	var looper int = 1
+	var sendSize float64 = math.Ceil(float64(size/10))
+	connection.Write([]byte(strconv.Itoa(int(sendSize))))
+	
+	if size <= 10 {
+		connection.Write(msg)
+	}else {
+		for i := 1; i < size; i++ {
+			tmpMsg[looper-1] += msg[i-1]
+			fmt.Println(msg[i])
+			if i % 10 == 0 {
+				fmt.Println(tmpMsg)
+				tmpMsg = make([]byte,10)
+				looper = 0
+				connection.Write(tmpMsg)
+			}else if size-1 == i {
+				fmt.Println(tmpMsg)
+				connection.Write(tmpMsg)
+			}
+			looper += 1
+		}
+	}
+}
 
 func client(){
 	_, port := getAddr()
+	fmt.Println(port)
 	//connection, err := net.Dial("tcp", (ip + ":" + port))
-	connection, err := net.Dial("tcp", ("130.237.227.22" + ":" + port))
+	connection, err := net.Dial("tcp", ("130.237.223.107" + ":" + "2345")) //connection is conn object
 
 	if err != nil {
 		fmt.Println(err)
@@ -45,31 +75,29 @@ func client(){
 	}
 
 	reader := bufio.NewReader(os.Stdin)
-	
 	for  {
-	
-	
-		msg, error := reader.ReadString('\n')
+		msg, error := reader.ReadBytes('\n')
+		fmt.Println(msg)
 		if( error != nil){
 			fmt.Println(err)
 			return
-		}
-		
-		_,writeError := connection.Write([]byte(msg))
+		}	
+	//	_,writeError := connection.Write([]byte(msg))
 
 		
-		if(writeError != nil){
+	/**	if(writeError != nil){
 			connection.Close()
 			fmt.Println(writeError)
 			return
 		}
+*/
+		write(connection,msg)
 
 		incmsg := make([]byte, 10)
 		bytes,_ := connection.Read(incmsg)
 		stringMsg := string(incmsg[:bytes])
+		fmt.Println(stringMsg)
+	
 		
-	//	if len(stringMsg) > 0 {
-			fmt.Println(stringMsg)
-	//	}
 	}
 }
