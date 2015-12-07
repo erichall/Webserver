aa51f796d89dcd466742a935977bc54c1359149a
Johannes Westlund <jwestlun@kth.se>
2015-12-07 17:27:51 +0100

    Added som functionality to client.

--- ATMClient.go
+++ ATMClient.go
@@ -1,20 +1,36 @@
 package main
 
+// Import packages.
 import (
 	"bufio"
 	"fmt"
 	"os"
 	"net"
-	//"strings"
+	"strings"
 	"encoding/binary"
 	"strconv"
-	//"math"
 )
 
+//Global variable that indicate how many language the client support.
+languages := "English","日本語","Deutsch"}
+var intro []string = ["Please pick a language.","言語を選択してください。","Bitte wählen Sie eine Sprache aus."]
+
+//Global variable that hold the language that the user chose.
+var text []string
+
+//main function that will create the client.
 func main() {
 	client()
 }
 
+func check(err error) {
+    if (err != nil) {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
+
+//read Reads from the server.
 func read(client net.Conn) (string, error) {
     holder := make([]byte, 10)
     number, err := client.Read(holder)
@@ -39,6 +55,7 @@ func read(client net.Conn) (string, error) {
     return message, nil
 }
 
+//getAddr gets the address.
 func getAddr() (string, string) {
 	var ip string
 	var port string
@@ -59,6 +76,7 @@ func getAddr() (string, string) {
 	return ip, port
 }
 
+//write Writes to the server.
 func write(connection net.Conn, msg []byte) {
 	size := binary.Size(msg)
 	sendSize := size/10
@@ -93,17 +111,62 @@ func write(connection net.Conn, msg []byte) {
 	
 }
 
+func languageConfig() {
+    var lang string
+    
+    for {
+        for _,elem := range intro {
+            fmt.Println(elem)
+        }
+        fmt.Println()
+        for _,elem := range languages {
+            fmt.Println(elem)
+        }
+        fmt.Scanf("%s", &lang)
+        lang = strings.TrimSpace(strings.ToLower(lang))
+        valid := false
+        
+        for _,elem := range languages {
+            if (strings.ToLower(elem) == lang) {
+                valid = true
+            }
+        }
+        if (valid) {
+            lang = lang + ".txt"
+            file, err := os.Open(lang)
+            defer file.Close()
+            check(err)
+
+            var lines []string
+            scanner := bufio.NewScanner(file)
+            for scanner.Scan() {
+		        lines += scanner.Text()
+	        }
+	        check(scanner.Err())
+            text = lines
+
+            break
+        } else {
+            fmt.Println("Sorry. I did not understand.")
+        }
+    }
+}
+
+//client starts the client.
 func client(){
-	_, port := getAddr()
-	fmt.Println(port)
-	connection, err := net.Dial("tcp", ("130.237.223.33" + ":" + "2345")) //connection is conn object
-
-
+	//_, port := getAddr()
+	//fmt.Println(port)
+    // Connect to the server through tcp/IP.
+	connection, err := net.Dial("tcp", ("130.237.223.33" + ":" + "2345"))
+    // If connection failed crash.
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 		return
 	}
+    languageConfig()
+    //Wait for server to send how the user can login.
+    read(connection)
 
 	reader := bufio.NewReader(os.Stdin)
 	for  {
