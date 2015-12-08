8a77316d9b91e87294e6522ce8f27855bcba4f8c
Johannes Westlund <jwestlun@kth.se>
2015-12-08 17:04:55 +0100

    Client works :)

--- ATMClient.go
+++ ATMClient.go
@@ -7,9 +7,6 @@ import (
 	"os"
 	"net"
 	"strings"
-	"encoding/binary"
-	"strconv"
-    "errors"
 )
 
 //Global variable that indicate how many language the client support.
@@ -34,29 +31,60 @@ func check(err error) {
     }
 }
 
-//read Reads from the server.
-func read(client net.Conn) (string, error) {
-    holder := make([]byte, 10)
-    number, err := client.Read(holder)
-    if (err != nil) {
-        return "", errors.New("Error couldn't get how many bytes that will be sent.")
-    }
-    bytes, conerr := strconv.Atoi(string(holder[0:number]))
-    if (conerr != nil) {
-        return "", errors.New("Could not convert data to byte.")
-    }
-    message := ""   
+func format(rest []byte) []byte { 
+    tmp := make([]byte, 10) 
+    for index := range tmp { 
+        if (index < len(rest)) { 
+            tmp[index] = rest[index] 
+        } else { 
+            tmp[index] = 32 
+        } 
+    } 
+    return tmp 
+}
 
-    for bytes != 0 {
-        letters, Rederr := client.Read(holder)
-        if (Rederr != nil) {
-            return "", errors.New("Error when reading from client.")
+//read Reads from the server.
+func write(connection net.Conn, msg []byte) {
+    size := len(msg)
+    sendSize := size/10
+    if size % 10 != 0 {
+        sendSize++
+    }
+
+    number := make([]byte, 1)
+    number[0] = byte(sendSize)
+    connection.Write(number)
+
+    for times := 1; times != sendSize; times++ {
+	    prev := 10*(times-1)
+	    _, timesError := connection.Write(msg[prev:(10*times)])
+	    check(timesError)
+    }
+     _, restError := connection.Write(format(msg[10*(sendSize-1):]))
+    check(restError)
+}
+
+func read(connection net.Conn) string {
+    times := make([]byte, 1)
+    for {
+        read, err := connection.Read(times)
+        check(err)
+        if read == 1 {
+            break
         }
+    }
+    bytes := int(times[0])
+    holder := make([]byte, 10)
+    message := ""
+    
+    for bytes != 0 {
+        letters, Rederr := connection.Read(holder)
+        check(Rederr)
         message += string(holder[0:letters])
         bytes--
     }
 
-    return message, nil
+    return strings.TrimSpace(message)
 }
 
 //getAddr gets the address.
@@ -80,108 +108,99 @@ func getAddr() (string, string) {
 	return ip, port
 }
 
-//write Writes to the server.
-func write(connection net.Conn, msg []byte) {
-	size := binary.Size(msg)
-	sendSize := size/10
-	if (size % 10 != 0) {
-		sendSize++
-	}
-	fmt.Println(sendSize)
-	_, sizeError := connection.Write([]byte(strconv.Itoa(sendSize)))
-
-	if sizeError != nil {
-		fmt.Println(sizeError)
-		os.Exit(1)
-		return
-	}
-
-	for times := 1; times != sendSize; times++ {
-		prev := 10*(times-1)
-		_, timesError := connection.Write(msg[prev:(10*times)])
-		if timesError != nil {
-			fmt.Println(timesError)
-			os.Exit(1)
-			return
-		}
-	}
-	_, restError := connection.Write(msg[10*(sendSize-1):])
-
-	if restError != nil {
-		fmt.Println(restError)
-		os.Exit(1)
-		return
-	}
-	
-}
-
-func languageConfig() {
-    var lang string
-    
+func languageConfig(connection net.Conn) {
+    intro := read(connection)
+    //check(introerr)
+    fmt.Println(intro)
     for {
-        for _,elem := range intro {
-            fmt.Println(elem)
-        }
-        fmt.Println()
-        for _,elem := range languages {
-            fmt.Println(elem)
-        }
-        fmt.Scanf("%s", &lang)
-        lang = strings.TrimSpace(strings.ToLower(lang))
-        valid := false
-        
-        for _,elem := range languages {
-            if (strings.ToLower(elem) == lang) {
-                valid = true
-            }
-        }
-        if (valid) {
-            lang = lang + ".txt"
-            file, err := os.Open(lang)
-            check(err)
-
-            var lines []string
-            scanner := bufio.NewScanner(file)
-            for scanner.Scan() {
-		        lines = append(lines, scanner.Text())
-	        }
-            file.Close()
-	        check(scanner.Err())
-            text = lines
-
+        picked := string(userInput())
+        picked = strings.ToLower(picked)
+        write(connection, []byte(picked))
+        answer := read(connection)
+        if answer == "approved" {
+            //fmt.Println("Let's break!")
             break
         } else {
-            fmt.Println("Sorry. I did not understand.")
+            fmt.Println(answer)
+        }
+    } 
+    //fmt.Println("Out of loop!")   
+}
+
+func userInput() []byte {
+    msg, err := reader.ReadBytes('\n')
+	check(err)
+    if len(msg) == 0 {
+        msg = append(msg, 32)
+    }
+    //fmt.Println(msg)
+    return msg
+}
+
+func loginSetUp(connection net.Conn) {
+    intro := read(connection)
+    cardText := read(connection)
+    fmt.Println(intro)
+    fmt.Print(cardText)
+    
+    for {
+        card := userInput()
+        write(connection, []byte(card))
+        fmt.Print(read(connection))
+        password := userInput()
+        write(connection, []byte(password))
+    
+        ans := read(connection)
+        if ans == "approved" {
+            fmt.Println(read(connection))
+            break
+        } else {
+            fmt.Print(ans)
         }
     }
 }
 
-func userInput() string {
-    msg, err := reader.ReadBytes('\n')
-	check(err)
-    return string(msg)
+func action(userInput string) {
+    
+}
+
+func handlingRequests(connection net.Conn) {
+    intro := read(connection) //What would you like to do?
+    options := read(connection) //(1)Balance, ...    
+    for {
+        fmt.Println(intro)
+        fmt.Println(options)
+        input := strings.TrimSpace(string(userInput()))
+        write(connection, []byte(input))
+        switch input {
+            case "1": //saldo
+                fmt.Println(read(connection))
+            case "2": //withdraw
+                fmt.Print(read(connection))
+                amount := strings.TrimSpace(string(userInput()))
+                write(connection, []byte(amount))
+            case "3": //deposit
+                fmt.Print(read(connection))
+                amount := strings.TrimSpace(string(userInput()))
+                write(connection, []byte(amount))
+            case "4": //exit
+                fmt.Println(read(connection))
+                os.Exit(0)
+            default:
+                fmt.Println(read(connection))           
+        }
+    }
 }
 
 //client starts the client.
 func client(){
     // Connect to the server through tcp/IP.
-	//connection, err := net.Dial("tcp", ("130.237.223.33" + ":" + "2345"))
+	connection, err := net.Dial("tcp", ("130.229.156.183" + ":" + "2333"))
     // If connection failed crash.
-	//check(err)
+	check(err)
     //Configure the language.
-    languageConfig()
-    //Wait for server to send how the user can login.
-    //login := read(connection)
-
-    fmt.Println(text[0])
-    fmt.Print(text[1])
-    cardnumber := userInput()
-    fmt.Println()
-    fmt.Print(text[2])
-    password := userInput()
-    
-    fmt.Println()
-    fmt.Println(text)
-    fmt.Println("Cardnumber = ", cardnumber)
-    fmt.Println("Password = ", password)
+    languageConfig(connection)
+    //Time to log in to the account.
+    loginSetUp(connection)
+    handlingRequests(connection)
 }
